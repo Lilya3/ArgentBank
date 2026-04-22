3256db2b67a7b48f2a0d284e038837474c5edfe2
Lilya3 <bazaralily4@gmail.com>
2026-04-22 16:43:22 +0200

    Step 2 : Setup Redux store + provider

---
+++ Frontend/argent-bank-frontend/src/store/userSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  token: null,
+  user: null,
+}
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setToken: (state, action) => {
+      state.token = action.payload
+    },
+    setUser: (state, action) => {
+      state.user = action.payload
+    },
+    logout: (state) => {
+      state.token = null
+      state.uer = null
+    },
+  },
+})
+
+export const { setToken, setUser, logout } = userSlice.actions
+export default userSlice.reducer
