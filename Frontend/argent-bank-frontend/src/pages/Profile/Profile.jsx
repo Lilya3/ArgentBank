8815bebca2be42c2ac65a62222910d6ea3588961
Lilya3 <bazaralily4@gmail.com>
2026-04-22 17:44:54 +0200

    Step 2: Auth: fetch profile + display user

--- Frontend/argent-bank-frontend/src/pages/Profile/Profile.jsx
+++ Frontend/argent-bank-frontend/src/pages/Profile/Profile.jsx
@@ -1,4 +1,51 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { setUser } from "../../store/userSlice"
+
+
 function Profile() {
-  return <h1>Profile</h1>
+
+  const dispatch = useDispatch()
+  const token = useSelector((state) => state.user.token)
+
+  const user = useSelector((state) => state.user.user)
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/api/v1/user/profile", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json"
+          },
+        })
+
+        if (!response.ok) {
+          throw new Error("Erreur récupération profil")
+        }
+
+        const data = await response.json()
+        dispatch(setUser(data.body))
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    if (token) {
+      fetchProfile()
+    }
+  }, [token, dispatch])
+
+  return (
+    <main className="main bg-dark">
+      <h1>
+        Welcome Back
+        <br />
+        {user?.firstName} {user?.lastName}
+      </h1>
+    </main>
+  )
 }
+
 export default Profile
