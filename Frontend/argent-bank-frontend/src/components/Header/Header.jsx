695c453d4d6c6d1d484b6f0e19a273069d6f4452
Lilya3 <bazaralily4@gmail.com>
2026-04-23 18:43:08 +0200

    Step 3 : Header: show username when logged in

--- Frontend/argent-bank-frontend/src/components/Header/Header.jsx
+++ Frontend/argent-bank-frontend/src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom'
 import logo from '../../assets/argentBankLogo.png'
+import { useSelector } from "react-redux"
 
 function Header() {
+  const token = useSelector((state) => state.user.token)
+  const user = useSelector((state) => state.user.user)
+
   return (
     <nav className="main-nav">
       <Link className="main-nav-logo" to="/">
@@ -14,9 +18,17 @@ function Header() {
       </Link>
 
       <div>
-        <Link className="main-nav-item" to="/sign-in">
+        <Link className="main-nav-item" 
+        to={token ? "/profile" : "/sign-in"}
+        >
+
           <i className="fa fa-user-circle"></i>
-          Sign In
+
+          {token ? (
+            <span>{user?.userName}</span>
+          ) : (
+            <span>Sign In</span>
+          )}
         </Link>
       </div>
     </nav>
--- Frontend/argent-bank-frontend/src/pages/Profile/Profile.jsx
+++ Frontend/argent-bank-frontend/src/pages/Profile/Profile.jsx
@@ -1,14 +1,58 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { setUser } from "../../store/userSlice"
 import { Navigate } from "react-router-dom"
+
 
 function Profile() {
   const dispatch = useDispatch()
   const token = useSelector((state) => state.user.token)
   const user = useSelector((state) => state.user.user)
 
+  const [isEditing, setIsEditing] = useState(false)
+  const [editedUserName, setEditedUserName] = useState("")
+
+  const handleEditClick = () => {
+    setEditedUserName(user?.userName || "")
+    setIsEditing(true)
+  }
+
+  const handleCancel = () => {
+    setIsEditing(false)
+    setEditedUserName(user?.userName || "")
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await fetch("http://localhost:3001/api/v1/user/profile", {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userName: editedUserName,
+        }),
+      })
+
+      if (!response.ok) {
+        throw new Error("Erreur update username")
+      }
+
+      const data = await response.json()
+      dispatch(setUser(data.body))
+      setIsEditing(false)
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   useEffect(() => {
+    if (!token) return
+
     const fetchProfile = async () => {
       try {
         const response = await fetch("http://localhost:3001/api/v1/user/profile", {
@@ -44,6 +88,23 @@ function Profile() {
         <br />
         {user?.firstName} {user?.lastName}
       </h1>
+
+      {isEditing ? (
+        <form onSubmit={handleSubmit}>
+          <input
+          type="text"
+          value={editedUserName}
+          onChange={(e) => setEditedUserName(e.target.value)}
+        />
+
+          <button type="submit">Save</button>
+          <button type="button" onClick={handleCancel}>
+            Cancel
+          </button>
+        </form>
+      ) : (
+        <button onClick={handleEditClick}>Edit Name</button>
+      )}
     </main>
   )
 }
